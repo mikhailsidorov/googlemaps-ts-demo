2ff0d4043e029f8e16ad6be56ee11946c88d0b01
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2020-12-23 01:54:33 +0300

    Refactor webpack config

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/, 
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(['GOOGLE_API_KEY']),
+    new HtmlWebpackPlugin({template: 'src/index.ejs'}),
+  ]
+}
